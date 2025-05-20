0b17e49632c4d86217734c07f9f46e1af4cc85b1
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2025-05-20 18:29:47 +0530

    feat(store): add custom hooks for typed useDispatch and useSelector

---
+++ src/store/hooks.ts
@@ -0,0 +1,10 @@
+// store/hooks.ts
+import {
+  type TypedUseSelectorHook,
+  useDispatch,
+  useSelector,
+} from 'react-redux';
+import type { RootState, AppDispatch } from './index';
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
