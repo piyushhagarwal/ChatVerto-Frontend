375e6a1d6dd2d518bf19084060d45fc61d12e3e5
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2025-05-20 18:19:16 +0530

    feat: restructure App component to include AuthGuard and routing for login and registration

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import "./App.css";
+import './App.css';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import { AuthGuard } from './components/AuthGuard';
+
+import Login from './features/auth/pages/loginPage';
+import Register from './features/auth/pages/registerPage';
 
 function App() {
+  // const baseURL = import.meta.env.VITE_API_BASE_URL;
   return (
-    <>
-      <h1 className="bg-orange-300">Hello </h1>
-    </>
+    <Router>
+      <AuthGuard>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </AuthGuard>
+    </Router>
   );
 }
 
