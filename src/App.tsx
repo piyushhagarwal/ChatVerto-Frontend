378ef5b9d7613e5049f06853cb90fc5f6b993080
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2025-06-09 16:16:43 +0530

    feat(app): add routing for dashboard and home pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import './App.css';
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom';
 
 import { AuthGuard } from './components/AuthGuard';
 
 import Login from './features/auth/pages/loginPage';
 import Register from './features/auth/pages/registerPage';
+import DashboardLayout from './features/dashboard/dashboardLayout';
+import HomePage from './features/home/pages/homePage';
 
 function App() {
   // const baseURL = import.meta.env.VITE_API_BASE_URL;
@@ -15,6 +22,11 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<DashboardLayout />}>
+            <Route index element={<Navigate to="home" replace />} />
+            <Route path="home" element={<HomePage />} />
+          </Route>
+          <Route path="/" element={<Navigate to="/dashboard/home" replace />} />
         </Routes>
       </AuthGuard>
     </Router>
