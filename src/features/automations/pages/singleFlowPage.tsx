e60280c4635e69bb0d8a43dc2d02318a6e1e575c
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2025-06-16 19:55:32 +0530

    feat(automations): implement SingleFlowPage with ReactFlow integration

--- src/features/automations/pages/singleFlowPage.tsx
+++ src/features/automations/pages/singleFlowPage.tsx
@@ -1,10 +1,87 @@
-import DashboardLayout from '@/features/dashboard/dashboardLayout';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useEffect, useCallback } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { ReactFlow, Controls, Background } from '@xyflow/react';
+import { useAppDispatch, useAppSelector } from '@/store/hooks';
+import {
+  fetchFlowByIdThunk,
+  createNewFlow,
+  saveFlowThunk,
+  renameFlow,
+  addEdgeToFlow,
+  applyNodeChangesAction,
+  applyEdgeChangesAction,
+} from '@/store/slices/flowsSlice';
+import { nodeTypes } from '@/types/node';
 
-export default function FlowsPage() {
+export default function SingleFlowPage() {
+  const { flowId } = useParams<{ flowId: string }>();
+
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const { currentFlow, loading, error } = useAppSelector(state => state.flow);
+  const nodes = currentFlow?.nodes || [];
+  const edges = currentFlow?.edges || [];
+
+  const onNodesChange = useCallback(
+    (changes: any) => {
+      dispatch(applyNodeChangesAction(changes));
+    },
+    [dispatch]
+  );
+
+  const onEdgesChange = useCallback(
+    (changes: any) => {
+      dispatch(applyEdgeChangesAction(changes));
+    },
+    [dispatch]
+  );
+
+  const onConnect = useCallback(
+    (params: any) => {
+      dispatch(addEdgeToFlow(params));
+    },
+    [dispatch]
+  );
+
+  useEffect(() => {
+    if (!flowId) return;
+
+    if (flowId.startsWith('temp-')) {
+      // Do nothing if it's a temporary flow because it is already called when creating a new flow from allFlowsPage
+    } else {
+      dispatch(fetchFlowByIdThunk(flowId));
+    }
+  }, [flowId, dispatch]);
+
+  useEffect(() => {
+    if (
+      currentFlow?.saved &&
+      currentFlow.id !== flowId // URL param id
+    ) {
+      navigate(`/flows/${currentFlow.id}`, { replace: true });
+    }
+  }, [currentFlow, flowId, navigate]);
+
+  const handleSave = () => {
+    dispatch(saveFlowThunk());
+  };
+
   return (
-    <div>
-      <DashboardLayout />
-      flowsPage
+    <div style={{ height: '100%' }}>
+      <ReactFlow
+        nodes={nodes}
+        onNodesChange={onNodesChange}
+        edges={edges}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        nodeTypes={nodeTypes}
+        fitView
+      >
+        <Background />
+        <Controls />
+      </ReactFlow>
     </div>
   );
 }
